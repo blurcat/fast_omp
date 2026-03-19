1f7d2937fac023761b226965fcdb05e6f0f8c107
黄飞 <huangfei@xinnet.com>
2026-03-19 14:48:35 +0800

    fix(config): 添加 extra='ignore' 允许 .env 中存在未定义字段

--- app/core/config.py
+++ app/core/config.py
@@ -14,6 +14,7 @@ class Settings(BaseSettings):
         env_file=".env",
         case_sensitive=True,
         env_list_delimiter=",",
+        extra="ignore",
     )
 
     PROJECT_NAME: str = "Ops Middle Platform"
